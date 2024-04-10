47405404390c355adf61067f96e762cad16e20cd
Tarun Kumar <tarunnimbal123@gmail.com>
2024-04-11 04:36:42 +0530

    Add userSchema for mongoDB in ts

--- src/model/user.model.ts
+++ src/model/user.model.ts
@@ -17,3 +17,54 @@ const messageSchema: Schema<Message> = new Schema({
             default:Date.now
           }
 })
+
+
+export interface user extends Document{
+       username:string,
+       email:string,
+       password:string,
+       verifyCode:string,
+       isVerified:boolean,
+       verifyCodeExpiry:Date,
+       isAcceptedMessage:boolean,
+       messages:Message[]
+}
+
+const userSchema:Schema<user> = new Schema({
+        username:{
+          type:String,
+          required:[true, "Username is required"],
+          trim:true,
+          unique:true
+        },
+        email:{
+          type:String,
+          required:[true, "email is  required field "],
+          unique:true,
+          match: [/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, "Please use a valid email address"]
+        },
+        password:{
+          type: String,
+          required:[true, "Password is required"],
+        },
+        verifyCode:{
+          type: String,
+          required:[true, "VerifyCode is required"],
+        },
+        isVerified:{
+          type: Boolean,
+        },
+        verifyCodeExpiry:{
+            type:Date,
+            required:[true, ""]
+        },
+        isAcceptedMessage:{
+          type:Boolean,
+          default:true
+        },
+        messages:[messageSchema]
+})
+
+const User = (mongoose.models.user as mongoose.Model<user>) || (mongoose.model<user>("User", userSchema));
+
+export default User;
